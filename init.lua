7892bb3f58dc0c0b1c26b814d43121e562712c69
Billre <wreese01@tampabay.rr.com>
2017-07-31 19:56:59 -0400

    Add more spawn privs

--- init.lua
+++ init.lua
@@ -1,23 +1,35 @@
 
 -- register privs for spawn points
-minetest.register_privilege("spawn1", {description = "Starting spawn.", give_to_singleplayer=false})
-minetest.register_privilege("spawn2", {description = "Second spawn.", give_to_singleplayer=false})
-minetest.register_privilege("spawn3", {description = "Third spawn.", give_to_singleplayer=false})
-minetest.register_privilege("spawn4", {description = "Fourth spawn.", give_to_singleplayer=false})
+minetest.register_privilege("farm", {description = "Farm spawn.", give_to_singleplayer=false})
+minetest.register_privilege("woods", {description = "Woods spawn.", give_to_singleplayer=false})
+minetest.register_privilege("mine", {description = "Mine spawn.", give_to_singleplayer=false})
+minetest.register_privilege("games", {description = "Games spawn.", give_to_singleplayer=false})
+minetest.register_privilege("spawn1", {description = "Area1 spawn.", give_to_singleplayer=false})
+minetest.register_privilege("spawn2", {description = "Area2 spawn.", give_to_singleplayer=false})
+minetest.register_privilege("spawn3", {description = "Area3 spawn.", give_to_singleplayer=false})
+minetest.register_privilege("spawn4", {description = "Area4 spawn.", give_to_singleplayer=false})
 
 
 -- function to select and move player to correct spawn
 local function respawn(name)
 	local spawnpos
 	-- check minetest.conf file for correct coordinates depending on privs	
-	if minetest.check_player_privs(name, {spawn4=true}) then
-		spawnpos = minetest.setting_get_pos("spawn_coordinate_4")
-	elseif minetest.check_player_privs(name, {spawn3=true}) then
-		spawnpos = minetest.setting_get_pos("spawn_coordinate_3")
-	elseif minetest.check_player_privs(name, {spawn2=true}) then
-		spawnpos = minetest.setting_get_pos("spawn_coordinate_2")
+	if minetest.check_player_privs(name, {farm=true}) then
+		spawnpos = minetest.setting_get_pos("spawn_coordinate_farm")
+	elseif minetest.check_player_privs(name, {woods=true}) then
+		spawnpos = minetest.setting_get_pos("spawn_coordinate_woods")
+	elseif minetest.check_player_privs(name, {mine=true}) then
+		spawnpos = minetest.setting_get_pos("spawn_coordinate_mine")
+	elseif minetest.check_player_privs(name, {games=true}) then
+		spawnpos = minetest.setting_get_pos("spawn_coordinate_games")	
 	elseif minetest.check_player_privs(name, {spawn1=true}) then
-		spawnpos = minetest.setting_get_pos("spawn_coordinate_1")
+		spawnpos = minetest.setting_get_pos("spawn_coordinate_Area1")
+	elseif minetest.check_player_privs(name, {spawn2=true}) then
+		spawnpos = minetest.setting_get_pos("spawn_coordinate_Area2")
+	elseif minetest.check_player_privs(name, {spawn3=true}) then
+		spawnpos = minetest.setting_get_pos("spawn_coordinate_Area3")
+	elseif minetest.check_player_privs(name, {spawn4=true}) then
+		spawnpos = minetest.setting_get_pos("spawn_coordinate_Area4")
 	end
 
 	-- return if no valid spawn position...
